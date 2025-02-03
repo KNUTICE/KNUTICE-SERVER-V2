09acde161343bee9f31f62077d4f2be78f8e768f
SEOB <svj7531@naver.com>
2025-02-03 14:25:25 +0900

    !HOTFIX: Fix Interceptor path pattern

--- api/src/main/java/api/common/config/web/WebConfig.java
+++ api/src/main/java/api/common/config/web/WebConfig.java
@@ -43,7 +43,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authorizationInterceptor)
-            .addPathPatterns("/**")
+            .addPathPatterns("/api/**")
             .excludePathPatterns(WHITE_LIST)
         ;
     }
