a85cf5d34044244e47ddaddc40c849088b86776d
SEOB <svj7531@naver.com>
2025-02-07 21:11:15 +0900

    !HOTFIX: CORS 허용
    !HOTFIX: CORS 허용

--- api/src/main/java/api/common/config/web/WebConfig.java
+++ api/src/main/java/api/common/config/web/WebConfig.java
@@ -10,6 +10,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -21,18 +22,21 @@ public class WebConfig implements WebMvcConfigurer {
 
     private final List<String> WHITE_LIST = List.of("/open-api/**");
 
-    private final List<String> URL = List.of("http://localhost:8001");
+    private final List<String> URL = List.of(
+        "http://localhost:8001",
+        "http://127.0.0.1:5500",
+        "https://www.knutice.site"
+    );
 
     private final List<String> METHODS = List.of("GET", "POST", "OPTIONS");
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(URL);
-        configuration.setAllowedMethods(METHODS);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // 모든 경로에 대해 CORS 적용
+            .allowedOrigins(URL.toArray(new String[0])) // 허용할 도메인
+            .allowedMethods(METHODS.toArray(new String[0])) // 허용할 HTTP 메서드
+            .allowedHeaders("*") // 모든 헤더 허용
+            .allowCredentials(true); // 인증 정보 포함 허용
     }
 
     @Override
