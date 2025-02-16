db29e79a82bf85de15135b926504302bc454db47
SEOB <svj7531@naver.com>
2025-02-16 22:47:03 +0900

    refactor: 북마크 동기화를 위한 API 개발
    refactor: 북마크 동기화를 위한 API 개발

--- api/src/main/java/api/domain/notice/controller/NoticeOpenApiController.java
+++ api/src/main/java/api/domain/notice/controller/NoticeOpenApiController.java
@@ -4,6 +4,7 @@ import api.domain.notice.business.NoticeBusiness;
 import api.domain.notice.controller.model.latestnotice.LatestThreeNoticeResponse;
 import api.domain.notice.controller.model.noticelist.NoticeRequest;
 import api.domain.notice.controller.model.noticelist.NoticeResponse;
+import api.domain.notice.controller.model.sync.NoticeSyncRequest;
 import global.api.Api;
 import jakarta.validation.Valid;
 import java.util.List;
@@ -14,6 +15,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -42,6 +45,15 @@ public class NoticeOpenApiController {
     @GetMapping("/{nttId}")
     public Api<NoticeResponse> getNotice(@PathVariable Long nttId) {
         NoticeResponse response = noticeBusiness.getNoticeBy(nttId);
+        return Api.OK(response);
+    }
+
+    // 북마크 Null 공백을 방지하기 위한 API 입니다.
+    @PostMapping("/sync")
+    public Api<List<NoticeResponse>> getNotice(
+        @RequestBody @Valid Api<NoticeSyncRequest> noticeSyncRequest
+    ) {
+        List<NoticeResponse> response = noticeBusiness.getNoticeList(noticeSyncRequest.getBody());
         return Api.OK(response);
     }
 
