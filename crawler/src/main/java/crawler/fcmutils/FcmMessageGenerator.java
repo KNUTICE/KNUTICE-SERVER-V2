e1e0098de8ebed5d6f285cf62d295ff2173876d2
SEOB <svj7531@naver.com>
2025-03-02 22:08:17 +0900

    refactor: FcmUtils 책임 분리
    refactor: FcmUtils 책임 분리

--- crawler/src/main/java/crawler/fcmutils/FcmMessageGenerator.java
+++ crawler/src/main/java/crawler/fcmutils/FcmMessageGenerator.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class MessageGenerator {
+public class FcmMessageGenerator {
 
     private final MessageFactory messageFactory;
 
