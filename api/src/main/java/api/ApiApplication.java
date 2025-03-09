2334c962533baa15700c32050627fcb42daec09b
SEOB <svj7531@naver.com>
2025-03-09 17:55:33 +0900

    feat: FeignClient 개발
    feat: FeignClient 개발

--- api/src/main/java/api/ApiApplication.java
+++ api/src/main/java/api/ApiApplication.java
@@ -2,7 +2,9 @@ package api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication(scanBasePackages = {"api", "db"})
 public class ApiApplication {
 
