be0b3cd2265df777e669fd282a9acbaf7e44dd03
SEOB <svj7531@naver.com>
2025-05-27 17:47:36 +0900

    test: API 모듈 테스트코드 구현
    test: API 모듈 테스트코드 구현

---
+++ api/src/test/java/api/domain/fcm/business/FcmTokenBusinessTest.java
@@ -0,0 +1,85 @@
+package api.domain.fcm.business;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import api.domain.fcm.controller.model.FcmTokenRequest;
+import api.domain.fcm.converter.FcmTokenConverter;
+import api.domain.fcm.service.FcmTokenService;
+import db.domain.token.fcm.FcmTokenDocument;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class FcmTokenBusinessTest {
+
+    @Mock
+    private FcmTokenService fcmTokenService;
+
+    @Mock
+    private FcmTokenConverter fcmTokenConverter;
+
+    @InjectMocks
+    private FcmTokenBusiness fcmTokenBusiness;
+
+    @Test
+    void 토큰_존재_저장_성공() {
+        // Given
+        String fcmToken = "fcmToken123";
+        FcmTokenRequest fcmTokenRequest = new FcmTokenRequest(fcmToken);
+
+        LocalDateTime now = LocalDateTime.now();
+
+        FcmTokenDocument existsFcmTokenDocument = FcmTokenDocument.builder()
+            .fcmToken(fcmToken)
+            .registeredAt(now)
+            .failedCount(0)
+            .build();
+
+        when(fcmTokenService.getFcmToken(fcmTokenRequest.getFcmToken())).thenReturn(
+            Optional.of(existsFcmTokenDocument));
+
+        when(fcmTokenService.saveFcmToken(existsFcmTokenDocument)).thenReturn(true);
+
+        // When
+        Boolean result = fcmTokenBusiness.saveFcmToken(fcmTokenRequest);
+
+        // Then
+        assertThat(result).isTrue();
+        assertThat(existsFcmTokenDocument.getFailedCount()).isEqualTo(0);
+        assertThat(existsFcmTokenDocument.getRegisteredAt()).isNotNull();
+        assertThat(existsFcmTokenDocument.getRegisteredAt()).isAfter(now);
+    }
+
+    @Test
+    void 토큰_없음_저장_성공() {
+        // Given
+        String fcmToken = "fcmToken123";
+        FcmTokenRequest fcmTokenRequest = new FcmTokenRequest(fcmToken);
+
+        FcmTokenDocument newFcmTokenDocument = FcmTokenDocument.builder()
+            .fcmToken(fcmToken)
+            .registeredAt(LocalDateTime.now())
+            .build();
+
+        when(fcmTokenService.getFcmToken(fcmTokenRequest.getFcmToken())).thenReturn(Optional.empty());
+        when(fcmTokenConverter.toDocument(fcmTokenRequest)).thenReturn(newFcmTokenDocument);
+        when(fcmTokenService.saveFcmToken(newFcmTokenDocument)).thenReturn(true);
+
+        // When
+        Boolean result = fcmTokenBusiness.saveFcmToken(fcmTokenRequest);
+
+        // Then
+        assertThat(result).isTrue();
+
+        verify(fcmTokenService, times(1)).getFcmToken(fcmToken);
+        verify(fcmTokenConverter, times(1)).toDocument(fcmTokenRequest);
+        verify(fcmTokenService, times(1)).saveFcmToken(newFcmTokenDocument);
+    }
+
+}
