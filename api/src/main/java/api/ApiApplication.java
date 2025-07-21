bad4ac270c5b43fb49ae6eedbfbcc11ad5e640f9
SEOB <svj7531@naver.com>
2025-07-21 15:32:59 +0900

    feat: Report 작성 시 Slack 알림 전송
    feat: Report 작성 시 Slack 알림 전송
    
    - Thread Pool 설정

--- api/src/main/java/api/ApiApplication.java
+++ api/src/main/java/api/ApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 
-@EnableAsync
 @EnableFeignClients
 @SpringBootApplication(scanBasePackages = {"api", "db"})
 public class ApiApplication {
